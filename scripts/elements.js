28fa1f80b7c45ec59a74949173a96bb8b23701fb
isaacBullinger <isaac.k.bullinger@gmail.com>
2025-05-19 23:44:07 -0500

    Added title with cursor animation. Added CSS computer monitor.

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,3 +1,9 @@
-document.getElementById("header").innerHTML = fetch("../header.html").then(response => response.text()).then(html => document.getElementById("header").innerHTML = html);
+fetch("../pages/components/header.html")
+    .then(response => response.text())
+    .then(
+        html => document.getElementById("header").innerHTML = html
 
-document.getElementById("footer").innerHTML = fetch("../footer.html").then(response => response.text()).then(html => document.getElementById("footer").innerHTML = html);
+        
+    );
+
+fetch("../pages/components/footer.html").then(response => response.text()).then(html => document.getElementById("footer").innerHTML = html);
